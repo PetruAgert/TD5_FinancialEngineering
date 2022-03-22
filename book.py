eea2281a7a35b91aa724b06663df92ae31e183b7
Tarek Boussofara <boussofaratech@gmail.com>
2022-03-22 17:03:55 +0100

    Book version 3

--- book.py
+++ book.py
@@ -1,43 +1,21 @@
-
 class Order:
-<<<<<<< HEAD
-	def __init__(self, quantity, price, buy=True):
-		self.__quantity = quantity
-		self.price = price
-		self.buy = buy
-	def quantity(self):
-		return self.__quantity if self.buy else -self.__quantity
-
-
-class Book:
-	def __init__(self,name):
-		self.name=name
-		self.orders=[]
-	def insert_buy (self,quantity_order,price_order):
-		order=Order(quantity_order,price_order,buy=True)
-		self.orders.append(order)
-
-	def insert_sell(self,quantity_order,price_order):
-		order=Order(self,quantity_order,price_order,buy=False)
-=======
-        def __init__(self, quantity, price, buy=True):
+        def __init__(self, quantity, price,buy=True):
                 self.__quantity = quantity
                 self.price = price
-                self.buy = buy
+                self.type=type
         def quantity(self):
                 return self.__quantity if self.buy else -self.__quantity
 
 
 class Book:
-        def __init__(self,name):
-                self.orders=[]
-		self.name = name
+        def __init__(self,name,orders=[]):
+                self.orders=orders
+                self.name = name
 
-        def insert_buy (self,quantity_order,price_order):
-                order=Order(quantity_order,price_order,buy=True)
+        def insert_buy (self,quantity,price):
+                order=Order(quantity,price,True)
                 self.orders.append(order)
 
-	def insert_sell(self, quantity_order, price_order):
-		order = Order(self, quantity_order, price_order, buy =False)
->>>>>>> 42c365746f7bec4ea1d9a3fe5542359f05b74955
-		self.orders.append(order)
+        def insert_sell(self, quantity, price):
+                order = Order(quantity, price,False)
+                self.orders.append(order)
