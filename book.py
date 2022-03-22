4cf1a6dd1a6cdc9eef9fdeca3ffbb69baa878013
PetruAgert <93982007+PetruAgert@users.noreply.github.com>
2022-03-22 16:21:12 +0100

    Update book.py

--- book.py
+++ book.py
@@ -1,6 +1,6 @@
 
 class Order:
-<<<<<<< HEAD
+
 	def __init__(self, quantity, price, buy=True):
 		self.__quantity = quantity
 		self.price = price
@@ -19,7 +19,7 @@ class Book:
 
 	def insert_sell(self,quantity_order,price_order):
 		order=Order(self,quantity_order,price_order,buy=False)
-=======
+
         def __init__(self, quantity, price, buy=True):
                 self.__quantity = quantity
                 self.price = price
@@ -28,16 +28,4 @@ class Book:
                 return self.__quantity if self.buy else -self.__quantity
 
 
-class Book:
-        def __init__(self,name):
-                self.orders=[]
-		self.name = name
 
-        def insert_buy (self,quantity_order,price_order):
-                order=Order(quantity_order,price_order,buy=True)
-                self.orders.append(order)
-
-	def insert_sell(self, quantity_order, price_order):
-		order = Order(self, quantity_order, price_order, buy =False)
->>>>>>> 42c365746f7bec4ea1d9a3fe5542359f05b74955
-		self.orders.append(order)
