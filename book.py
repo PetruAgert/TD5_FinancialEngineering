bf0a5100ffcdce0968e2df2bd78c10741ad352a0
Tarek Boussofara <boussofaratech@gmail.com>
2022-03-22 16:12:09 +0100

    version 1

---
+++ book.py
@@ -0,0 +1,21 @@
+
+class Order:
+	def __init__(self, quantity, price, buy=True):
+		self.__quantity = quantity
+		self.price = price
+		self.buy = buy
+	def quantity(self):
+		return self.__quantity if self.buy else -self.__quantity
+
+
+class Book:
+	def __init__(self,name):
+		self.name=name
+		self.orders=[]
+	def insert_buy (self,quantity_order,price_order):
+		order=Order(quantity_order,price_order,buy=True)
+		self.orders.append(order)
+
+	def insert_sell(self,quantity_order,price_order):
+		order=Order(self,quantity_order,price_order,buy=False)
+		self.orders.append(order)
