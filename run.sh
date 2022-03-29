7309044d7ce19f78bca74f8967975fffdfdd2551
Petru-Luigi Agert <petru-luigi.agert@edu.devinci.fr>
2022-03-29 22:39:16 +0200

    run.sh

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+if [[ -d ./.venv ]]
+then
+	source ./.venv/bin/activate
+	alias activate=". ../.env/bin/activate"
+	pipenv install -r requirements.txt
+fi
+
+pip install -r requirements.txt
+python3 main.py
